5af2f1653648fed2e4a5e3c003837b88708b2aa7
RaulAlex08 <96210733+RaulAlex08@users.noreply.github.com>
2021-12-15 23:45:20 +0100

    Add files via upload

---
+++ SmartVest/SmartVest.ino
@@ -0,0 +1,355 @@
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
+#include <Adafruit_GPS.h>
+#include <ArduinoOTA.h>
+#include <Thread.h>
+//#include <SoftwareSerial.h>
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
+
+#include "config.h" //modificar para ssid y passwd
+#include "ESP32_Utils.hpp"
+#include "ESP32_Utils_OTA.hpp"
+
+WiFiClient thingsBoard;
+PubSubClient client(thingsBoard);
+
+const char* mqtt_server = "demo.thingsboard.io";
+const char* TOPIC_T = "v1/devices/me/telemetry";
+const char* TOPIC_A = "v1/devices/me/attributes";
+const char* token_sensor = "ezFZ9fWSbSBbCVMynTFG";
+const char* token_pulsador = "ptERkUKriZQLQ2Pheiqu";
+const char* token_tracker = "xtq1k3JGjoGNJt86hY0m";
+const char* token_id_user = "218963084";
+#define TOKEN_BOT "5013259796:AAHe5eJAZU4J9vY1FBdiAdoVF-eB88GUY9w"//"2065123278:AAHlaf3gKAg11jGD6aJQuq_hXFX65rE6MZc-684890990" //"5013259796:AAHe5eJAZU4J9vY1FBdiAdoVF-eB88GUY9w"
+
+// Sensor 1
+const byte trig1 = 12; //Pin GPIO12 para el Trigger
+const byte echo1 = 14; //Pin GPIO14 para el Echo
+// Sensor 2
+const byte trig2 = 27; //Pin GPIO27 para el Trigger
+const byte echo2 = 26; //Pin GPIO26 para el Echo
+// Zumbador
+const byte zumbador = 25; //Pin GPIO25 para Zumbador
+// Pulsador
+const byte pulsador = 33; //Pin GPIO33 para Interrup del Pulsador
+// GPS
+#define GPSSerial Serial2 //UART 2 GPIO16 Rx, GPIO17 Tx
+Adafruit_GPS GPS(&GPSSerial); // Connect to the GPS on the hardware port
+// Bot Telegram
+WiFiClientSecure espClient;
+UniversalTelegramBot bot(TOKEN_BOT,espClient);
+// Threads
+Thread gpsThread = Thread(); //Thread para el GPS
+Thread botThread = Thread(); // Thread para el Bot de Telegram
+
+long dist1; //distancia en cm
+long dist2; //distancia en cm
+float latitud; // coordenadas latitud
+float longitud; // coordenadas longitud
+bool ubiOK = false; //Bool para verificar la correcta lectra del GPS
+int alturaUser = 175; // Valor por default de la altura
+bool commandAltura = false; // Bool para saber si ha introducido el comando /altura en el chatbot
+volatile bool sos = false; //auxiliar para la interrupcion del botón
+uint32_t timer = millis(); //timer para mostrar la ubicación del GPS
+
+/****************************************************************************************
+ *                                  SETUP                                               *
+ ****************************************************************************************/
+void setup() {
+  Serial.begin(115200);
+  
+  espClient.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
+  ConnectWiFi_STA();
+  InitOTA();
+
+  client.setServer(mqtt_server,1883);
+  
+  pinMode(trig1, OUTPUT);
+  pinMode(echo1, INPUT);
+  digitalWrite(trig1,LOW);
+
+  pinMode(trig2, OUTPUT);
+  pinMode(echo2, INPUT);
+  digitalWrite(trig2,LOW); 
+
+  pinMode(zumbador,OUTPUT);
+  
+  pinMode(pulsador, INPUT_PULLUP);
+  attachInterrupt(pulsador, interrupt_SOS, FALLING); //Está en low porque tenemos un interruptor y para hacer las pruebas
+
+  GPS.begin(9600);
+  // Turn on RMC (recommended minimum) and GGA (fix data) including altitude
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  // Set the update rate
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
+  // Request updates on antenna status, comment out to keep quiet
+  GPS.sendCommand(PGCMD_ANTENNA);
+  delay(1000);
+  // Ask for firmware version
+  GPSSerial.println(PMTK_Q_RELEASE);
+
+  gpsThread.onRun(lecturaUbi);
+  gpsThread.setInterval(1000); //cada 1s se llama a calcular la ubicacion GPS
+
+  bot_setup();
+  
+  botThread.onRun(checkMessages);
+  botThread.setInterval(5000); // cada 5s comprueba los mensajes del chatbot
+
+}
+
+
+/****************************************************************************************
+ *                                  LOOP                                                *
+ ****************************************************************************************/
+void loop() {
+
+  ArduinoOTA.handle();
+  client.loop();
+
+  if(botThread.shouldRun())
+    botThread.run();
+  
+  dist1 = lecturaDistancia(trig1,echo1);
+  dist2 = lecturaDistancia(trig2,echo2);  
+  avisoZumb(dist1);
+  avisoZumb(dist2);
+  
+  mandarDistancia(1,dist1);
+  mandarDistancia(2,dist2);
+
+  if(gpsThread.shouldRun())
+    gpsThread.run();
+    
+  if(ubiOK){
+    mandarUbi();
+    ubiOK = false;
+  }
+  
+  mandarPulsadorSOS();
+  
+  //delay (1000);
+}
+
+
+/****************************************************************************************
+ *                                  FUNCIONES                                           *
+ ****************************************************************************************/
+
+// ******************************* LECTURA DISTANCIA
+long lecturaDistancia(byte trig, byte echo){
+  long t;
+  
+  digitalWrite(trig,HIGH);
+  delayMicroseconds(10); // Eviamos un pulso de 10us
+  digitalWrite(trig,LOW);
+  t = pulseIn(echo,HIGH); //Obtenemos el ancho del pulso
+  return t/59; //Escalamos el tiempo a una distancia en cm
+}
+
+// ************************ ZUMBADOR
+void avisoZumb(long dist){
+  if(dist < 150){
+    Serial.println("Zumbador ON");
+    for(int i=0;i<2;i++){
+      digitalWrite(zumbador,HIGH);
+      delay(100);
+      digitalWrite(zumbador,LOW);
+      delay(100);
+    }
+    digitalWrite(zumbador,LOW);
+  }
+}
+
+//************************************************ LECTURA UBI
+void lecturaUbi(){
+  while(true){
+    // read data from the GPS in the 'main loop'
+    GPS.read();
+    // if a sentence is received, we can check the checksum, parse it...
+    if (GPS.newNMEAreceived()) {
+      if (!GPS.parse(GPS.lastNMEA())) // this also sets the newNMEAreceived() flag to false
+        return; // we can fail to parse a sentence in which case we should just wait for another
+    }
+  
+    // approximately every 20 seconds or so, print out the current stats
+    if (millis() - timer > 20000) {
+      timer = millis(); // reset the timer
+      Serial.print("Fix: "); Serial.print((int)GPS.fix);
+      Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
+      if (GPS.fix) {
+        Serial.print("Location: ");
+        int aux_lat_grados = GPS.latitude/100;
+        float aux_lat_minutos = (float)GPS.latitude - (aux_lat_grados * 100);
+        latitud = aux_lat_grados + (float)(aux_lat_minutos/60);
+        Serial.print("Latitud: ");
+        Serial.print(latitud);
+        Serial.print(", "); 
+        int aux_lon_grados = GPS.longitude/100;
+        float aux_lon_minutos = (float)GPS.longitude - (aux_lon_grados * 100);
+        longitud = (-1) * (aux_lon_grados + (float)(aux_lon_minutos/60));
+        Serial.print("Longitud: ");
+        Serial.println(longitud);
+        ubiOK = true;
+      }
+    }
+  }
+}
+
+//******************************** MANDAR DISTANCIA
+void mandarDistancia(int sensor, int distancia){
+  char buf2[50];
+  const char* fin = "}";
+  String buf;
+
+  if (client.connect("Sensor Distancia",token_sensor,"")) {
+      Serial.println("Connected Sensor Distancia");
+    }
+
+  if(sensor == 1){ // 1 - Sensor de arriba
+    buf = "{\"distance_TS\":";
+
+    buf.concat(distancia);
+    buf.concat(fin);
+    Serial.print("Enviado: ");
+    Serial.println(buf);  
+    buf.toCharArray(buf2,50);
+    if(client.publish(TOPIC_T,buf2)){
+      Serial.println("OK");
+    }
+  }
+  else if (sensor == 2){ // 2 - Sensor de abajo
+    buf = "{\"distance_BS\":";
+    buf.concat(distancia);
+    buf.concat(fin);
+    Serial.print("Enviado: ");
+    Serial.println(buf);
+    buf.toCharArray(buf2,50);
+    if(client.publish(TOPIC_T,buf2)){
+      Serial.println("OK");
+    }
+  }
+  client.disconnect();
+} 
+
+//************************** MANDAR PULSADOR
+void mandarPulsadorSOS(){
+  String buf = "{\"SOS\":";
+  const char* fin ="}"; 
+  char buf2[50];
+  String puls = "false";
+  Serial.println("Mandar Pulsador:");
+  if (client.connect("SOS",token_pulsador,"")) {
+    Serial.println("Connected Pulsador");
+  }
+  if(sos){
+    puls = "true";
+  }
+  buf.concat(puls);
+  buf.concat(fin);
+  Serial.print("Enviado: ");
+  Serial.println(buf);
+  buf.toCharArray(buf2,50);
+  if(client.publish(TOPIC_A,buf2)){
+    Serial.println("OK");
+  }
+  client.disconnect();
+  sos = false;
+}
+
+// *********************** MANDAR UBI
+void mandarUbi(){  
+  String buf;
+  char buf2[100];
+  buf = "{\"latitude\":";
+  String buf_aux =",\"longitude\":"; 
+  const char* fin = "}";
+  Serial.println("Mandar Ubi:");
+  if (client.connect("Tracker",token_tracker,"")) {
+    Serial.println("Connected Tracker");
+  }
+  buf.concat(latitud); //latitud 
+  buf_aux.concat(longitud); //longitud
+  buf.concat(buf_aux);
+  buf.concat(fin);
+  Serial.print("Enviado: ");
+  Serial.println(buf);
+  buf.toCharArray(buf2,100);
+  if(client.publish(TOPIC_A,buf2)){
+    Serial.println("OK");
+  }
+  client.disconnect();
+}
+
+//*********************************** BOT SETUP
+void bot_setup()
+{
+  const String commands = F("["
+                            "{\"command\":\"help\",  \"description\":\"Get bot usage help\"},"
+                            "{\"command\":\"start\", \"description\":\"Message sent when you open a chat with a bot\"},"
+                            "{\"command\":\"interrupt\", \"description\":\"Interrupcion Manual para simulación de boton SOS\"},"
+                            "{\"command\":\"setAltura\",\"description\":\"Para introducir la altura del Usuario\"}" // no comma on last command
+                            "]");
+  bot.setMyCommands(commands);
+}
+//******************************** HANDLE NEW MESSAGES
+void handleNewMessages(int numNewMessages){
+  Serial.println("handleNewMessages");
+  Serial.println(numNewMessages);
+  String answer;
+  for(int i = 0; i < numNewMessages; i++){
+    telegramMessage &msg = bot.messages[i];
+    String chat_id = String(bot.messages[i].chat_id);
+
+    Serial.println(msg.text);
+    //String from_name = bot.messages[i].from_name;
+    if(msg.text == "/start"){
+      answer = "Bienvenido a SmartVest.\n"; //+ from_name +
+      answer += "Usa el comando /help para ver los comandos.\n";
+    }
+    else if(msg.text == "/help"){
+      answer = "/start : Comando inicial para mensaje de bienvenida.\n";
+      answer = "/help : Comando para mostrar todos los comandos.\n";
+      answer += "/setAltura : Comando para introducir la altura del usuario, en cm. Ej: 175.\n";
+      answer += "/interrupt : Comando para simular el boton SOS (solo en prototipo).";
+    }
+    else if(msg.text == "/setAltura"){
+      commandAltura = true;
+      answer =  "Introduce la altura.";
+    }
+    else if(msg.text == "/interrupt"){
+      sos = true;
+      answer =  "Interrupción Manual.";
+    }
+    else if(commandAltura){
+      commandAltura = false;
+      alturaUser = msg.text.toInt();
+      answer = "Altura introducida correctamente con valor: " + String(alturaUser);
+    }
+    else
+      answer = "No te he entendido.";
+      
+    bot.sendMessage(msg.chat_id, answer, "Markdown");
+  }
+}
+
+//******************************************COMPROBAR MSG
+void checkMessages(){
+  Serial.println("Check");
+  int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+  while(numNewMessages){
+    Serial.print("got response");
+    handleNewMessages(numNewMessages);
+    numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+  }
+}
+
+/******************************************************************** 
+ *                        INTERRUPT                                 *
+ ********************************************************************/
+void interrupt_SOS(){
+  Serial.println("Interrupt");
+  sos = true;
+}
---
+++ SmartVest/config.h
@@ -0,0 +1,7 @@
+const char* ssid     = "AndroidAP9005";
+const char* password = "44dd025adb09";
+const char* hostname = "ESP32_SmartVest";
+
+IPAddress ip(192, 168, 1, 200);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
